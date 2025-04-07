ab330ec728286f56473deeec0358f988f9345c58
Artem Bilousov <artem.bilousov.1993@gmail.com>
2025-04-07 19:34:50 +0200

    updated contactsRouter.js

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -5,6 +5,7 @@ import {
   deleteContact,
   createContact,
   updateContact,
+  updateFavorite,
 } from "../controllers/contactsControllers.js";
 
 import validateBody from "../helpers/validateBody.js";
@@ -24,5 +25,7 @@ contactsRouter.delete("/:id", deleteContact);
 contactsRouter.post("/", validateBody(createContactSchema), createContact);
 
 contactsRouter.put("/:id", validateBody(updateContactSchema), updateContact);
+
+contactsRouter.patch("/:id/favorite", updateFavorite);
 
 export default contactsRouter;
