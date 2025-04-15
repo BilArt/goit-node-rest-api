1c3eda4034fddf790683588b22c88c16555efb2a
Artem Bilousov <artem.bilousov.1993@gmail.com>
2025-04-15 19:23:35 +0200

    updated contactsRouter.js and contactsShemas.js

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -8,24 +8,21 @@ import {
   updateFavorite,
 } from "../controllers/contactsControllers.js";
 
+import auth from "../middlewares/auth.js";
 import validateBody from "../helpers/validateBody.js";
 import {
   createContactSchema,
   updateContactSchema,
+  updateFavoriteSchema,
 } from "../schemas/contactsSchemas.js";
 
 const contactsRouter = express.Router();
 
-contactsRouter.get("/", getAllContacts);
-
-contactsRouter.get("/:id", getOneContact);
-
-contactsRouter.delete("/:id", deleteContact);
-
-contactsRouter.post("/", validateBody(createContactSchema), createContact);
-
-contactsRouter.put("/:id", validateBody(updateContactSchema), updateContact);
-
-contactsRouter.patch("/:id/favorite", updateFavorite);
+contactsRouter.get("/", auth, getAllContacts);
+contactsRouter.get("/:id", auth, getOneContact);
+contactsRouter.delete("/:id", auth, deleteContact);
+contactsRouter.post("/", auth, validateBody(createContactSchema), createContact);
+contactsRouter.put("/:id", auth, validateBody(updateContactSchema), updateContact);
+contactsRouter.patch("/:id/favorite", auth, validateBody(updateFavoriteSchema), updateFavorite);
 
 export default contactsRouter;
